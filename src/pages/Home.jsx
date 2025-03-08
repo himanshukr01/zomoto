60659a370a30ac285648772b509a6115f54b7492
Himanshu kumar <himanshukr1505892@gmail.com>
2025-03-08 23:48:17 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,23 +20,26 @@ const Home = () => {
     <div className="container">
       <Navbar />
       <div>
-        <img style={{ width: '30%', position: 'fixed', top: 150, left: 20, zIndex: 1000 }} src="burger.jpg" alt="" />
-        <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 150, zIndex: 1000 }} >Burger</h2>
-      </div>
-      <div>
-        <img style={{ width: '30%', position: 'fixed', top: 150, left: 500, zIndex: 1000 }} src="burger.jpg" alt="" />
-        <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 650, zIndex: 1000 }} >Burger</h2>
-      </div><div>
-        <img style={{ width: '30%', position: 'fixed', top: 150, left: 980, zIndex: 1000 }} src="burger.jpg" alt="" />
-        <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 1150, zIndex: 1000 }} >Burger</h2>
-      </div><div>
-        <img style={{ width: '30%', position: 'fixed', top: 150, left: 1460, zIndex: 1000 }} src="burger.jpg" alt="" />
-        <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 1650, zIndex: 1000 }} >Burger</h2>
+        <div>
+          <img  style={{ width: '30%', position: 'fixed', top: 150, left: 20, zIndex: 1000 }} src="burger.jpg" alt="" />
+          <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 150, zIndex: 1000 }} >Burger</h2>
+        </div>
+        <div>
+          <img style={{ width: '30%', position: 'fixed', top: 150, left: 500, zIndex: 1000 }} src="noodles.jpg" alt="" />
+          <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 650, zIndex: 1000 }} >Noodles</h2>
+        </div><div>
+          <img style={{ width: '30%', position: 'fixed', top: 150, left: 980, zIndex: 1000 }} src="pizza.jpg" alt="" />
+          <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 1150, zIndex: 1000 }} >Pizza</h2>
+        </div><div>
+          <img style={{ width: '30%', position: 'fixed', top: 150, left: 1460, zIndex: 1000 }} src="wine.jpg" alt="" />
+          <h2 style={{ width: '30%', position: 'fixed', top: 450, left: 1650, zIndex: 1000 }} >Grapes Wine</h2>
+        </div>
       </div>
       <button style={{ width: '10%', position: 'fixed', top: 60, left: 1220, zIndex: 1000 }} onClick={() => {
         localStorage.removeItem('username');
         navigate('/login');
       }}>Login</button>
+
     </div>
   )
 }
